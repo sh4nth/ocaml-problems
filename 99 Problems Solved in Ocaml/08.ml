57f4e42e6362f15bde0da0e4c2c878e50e237bd0
Shanthanu Bhardwaj <sbhardwaj@palantir.com>
2015-07-11 16:11:32 -0700

    Better solution to 08

--- 99 Problems Solved in Ocaml/08.ml
+++ 99 Problems Solved in Ocaml/08.ml
@@ -1,12 +1,19 @@
 (* Eliminate consecutive duplicates of list elements. *)
 
-let compress x = 
+
+(* let compress x = 
     let rec aux acc lst last = match lst with
     [] -> acc
     | head::rest -> if ((Some head) = last) then aux acc rest last
                                             else aux (head::acc) rest (Some
                                             head) 
     in List.rev ( aux [] x None) ;;
+Better pattern matching: *)
 
+let rec compress = function
+    | a::(b::c as t) -> if a = b then compress t else a::(compress t )
+    | smaller -> smaller ;;
+
+compress ["a"; "a"];;
 compress ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
 
